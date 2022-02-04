734ef0dabc24aee30671ab91bcd6b4c1288040e2
JonCabot <jfc0053@gmail.com>
2022-02-04 14:23:55 -0600

    completed assignment

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -2,20 +2,20 @@ import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
-  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [selectedCategory, setSelectedCategory] = useState("All");  //intial State is "Filter by Category", but use <option> "value" name = "All"
 
   function selectHandler(event) {
-    setSelectedCategory(event.target.value)
+    setSelectedCategory(event.target.value) //set State to this item's value, returns category; "Produce", etc..
   }
 
   const itemsToDisplay = items.filter((item) => {
-      if(selectedCategory === "All") {
+      if(selectedCategory === "All") { //if the current State = "Filter by Categoy", then return that
         return true;
       } else {
-        return item.category === selectedCategory
+        return item.category === selectedCategory //otherwise, return the category of the selected item.  The filter does this for each item matching that category.
       };
   });
-
+  //map below renders the category filter to the page, displaying each item with their props.
   return (
     <div className="ShoppingList">
       <div className="Filter">
